c0a2874080a0309cf379a45fc1d5b020ff9bfe7a
Abderraouf Rahmani <abderraouf.dev@gmail.com>
2023-02-08 23:45:44 +0100

    added examples🎁, and optimise the algo even more 🚀🚀

--- CountingValleys.java
+++ CountingValleys.java
@@ -1,15 +1,17 @@
 
 class CountingValleys {
     public static void main(String[] args) {
-        // DDDUUDUUUDDUU 13  not at sea level
+        // DDDUUDUUUDDUU 13   ---->     Expected Output: not at sea level \n 2
+        // UDDDUDUU 8         ---->     Expected Output: 1
+        // DDUUDDUDUUUD 12    ---->     Expected Output: 2
+        // DUDDDUUDUU 10      ---->     Expected Output: 2
         
         String s = "DDDUUDUUUDDUU";
-        int n = 13;
         char[] arrayOfSteps = s.toCharArray();
         int level = 0;
         int numOfVall = 0;
 
-        if ( n < 2 || n > 1000000) {
+        if ( s.length() < 2 || s.length() > 1000000) {
             System.out.print("The number of steps (n) must be greater or equal to 2 and less or equal to 10^6!");
             return;
         }
